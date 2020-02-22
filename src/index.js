e10c63309849c3755072a5090d016785687a1b8c
rtxrulez <>
2020-02-22 12:28:48 +0300

    init: Первая версия функции

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    return parseInt(
+        str
+            .split("")
+            .reverse()
+            .join("")
+    );
+};
